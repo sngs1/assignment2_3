ae3991b5a6a33ae195f7c907bf00be28d91bd835
GANGA C K <kochupotty@gmail.com>
2016-11-03 21:18:05 +0530

    as_2_3

---
+++ a23.java
@@ -0,0 +1,28 @@
+package a4;
+
+public class a23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1;i<6;i++)
+		{
+			if(i==1||i==5)
+			{
+			System.out.println("  a  ");
+			}
+			else if (i==2||i==4)
+			{
+				System.out.println(" aba ");
+				
+			}
+			else
+			{
+				System.out.println("abcba");
+			}
+						
+		}
+
+	}
+
+}
